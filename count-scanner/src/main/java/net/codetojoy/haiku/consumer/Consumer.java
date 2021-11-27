819c0113ae9dd4b1e45ab2f2be85618fa0d797bf
Michael Easter <codetojoy@gmail.com>
2021-11-27 10:14:02 -0400

    remove Spring Boot stuff, in prep for Micronaut

--- count-scanner/src/main/java/net/codetojoy/haiku/consumer/Consumer.java
+++ count-scanner/src/main/java/net/codetojoy/haiku/consumer/Consumer.java
@@ -5,6 +5,8 @@ import net.codetojoy.haiku.common.*;
 
 import java.util.List;
 
+// TODO: consider a stream and use Java's Consumer interface
+
 public interface Consumer<T> {
     List<Result> consume(Queue<T> queue);
 }
