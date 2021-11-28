30f8936575eea7deae2fa017b2bfde7c32de0259
Michael Easter <codetojoy@gmail.com>
2021-11-28 19:57:00 -0400

    clean up

--- count-scanner/src/main/java/net/codetojoy/haiku/controller/ScanCountController.java
+++ count-scanner/src/main/java/net/codetojoy/haiku/controller/ScanCountController.java
@@ -3,22 +3,16 @@ package net.codetojoy.haiku.controller;
 import io.micronaut.http.*;
 import io.micronaut.http.annotation.*;
 
+import java.net.http.*;
 import java.util.List;
 
 import net.codetojoy.haiku.common.*;
 import net.codetojoy.haiku.consumer.*;
 import net.codetojoy.haiku.producer.*;
 
-record DataList(List<Entry> data) {
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        for (var d : data) {
-            builder.append(d + "\n");
-        }
-        return builder.toString();
-    }
-}
+// TODO: consider generics
+record DataList(List<Entry> data) {}
+record ResultList(List<Result> data) {}
 
 @Controller("/scan-count")
 public class ScanCountController {
@@ -29,30 +23,16 @@ public class ScanCountController {
     }
 
     @Post("/find")
-    public String find(@Body DataList dataList) {
-        System.out.println("TRACER v3 param dataList: " + dataList.toString());
-        var builder = new StringBuilder();
+    public ResultList find(@Body DataList dataList) {
+        var version = "v5";
+        System.out.println("TRACER " + version + " param dataList: " + dataList.toString());
         var queue = new SimpleArrayQueue<Entry>();
 
         var producer = new HttpProducer(dataList.data());
         producer.produce(queue);
         var consumer = new SimpleConsumer();
         var results = consumer.consume(queue);
-
-        // TODO: use a JSON serializer
-        var numResults = results.size();
-        var i = 1;
-        builder.append("{\"data\": [");
-        for (var result : results) {
-            builder.append(result.toJson());
-
-            if (i < numResults) {
-                builder.append(",");
-            }
-            i++;
-        }
-        builder.append("]}");
-
-        return builder.toString();
+        var resultList = new ResultList(results);
+        return resultList;
     }
 }
